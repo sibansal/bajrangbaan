0b737ed0175a6470ff142726df9a8b89ddcc71db
Simran Bansal <contributions@sibansal.dev>
2023-07-01 21:16:56 +0530

    Added Sankat Mochan, Hanuman Chalisha and Hanuman Aarti to the app...

--- app/src/main/java/dev/sibansal/bajrangbaan/MainActivity.java
+++ app/src/main/java/dev/sibansal/bajrangbaan/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,10 +23,104 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.chapter_canvas);
         TextView textCanvas = initTextCanvas();
+        hideChapterMenu();
+        initChapterMenu();
         initZoomControls(textCanvas);
-        loadText(textCanvas);
+        loadBajrangBaan();
+        initChapterButtons();
+    }
+
+    private void initChapterButtons()
+    {
+        Button bajrangBaan = findViewById(R.id.bajrangBaan);
+        Button hanumanChalisa = findViewById(R.id.hanumanChalisa);
+        Button hanumanAarti = findViewById(R.id.hanumanAarti);
+        Button sankatMochan = findViewById(R.id.sankatMochan);
+
+        bajrangBaan.setOnClickListener(view -> {
+            loadBajrangBaan();
+            hideChapterMenu();
+        });
+
+        hanumanChalisa.setOnClickListener(view -> {
+            loadHanumanChalisa();
+            hideChapterMenu();
+        });
+
+        hanumanAarti.setOnClickListener(view -> {
+            loadHanumanAarti();
+            hideChapterMenu();
+        });
+
+        sankatMochan.setOnClickListener(view -> {
+            loadSankatMochan();
+            hideChapterMenu();
+        });
+    }
+
+    private void loadBajrangBaan()
+    {
+        TextView textCanvas = findViewById(R.id.textCanvas);
+        String textFile="bajrang_baan.txt";
+        setChapterTitle("बजरंग बाण");
+        loadText(textCanvas,textFile);
+    }
+
+    private void loadHanumanChalisa()
+    {
+        TextView textCanvas = findViewById(R.id.textCanvas);
+        String textFile="hanuman_chalisa.txt";
+        setChapterTitle("हनुमान चालीसा");
+        loadText(textCanvas,textFile);
+    }
+    private void loadHanumanAarti()
+    {
+        TextView textCanvas = findViewById(R.id.textCanvas);
+        String textFile="aarati.txt";
+        setChapterTitle("हनुमान आरती");
+        loadText(textCanvas,textFile);
+    }
+    private void loadSankatMochan()
+    {
+        TextView textCanvas = findViewById(R.id.textCanvas);
+        String textFile="sankat_mochan.txt";
+        setChapterTitle("संकट मोचन");
+        loadText(textCanvas,textFile);
+    }
+
+    private void setChapterTitle(String chapterTitle)
+    {
+        TextView title=findViewById(R.id.chapterTitle);
+        title.setText(chapterTitle);
+    }
+
+    private void initChapterMenu()
+    {
+        Button chapterMenu = findViewById(R.id.chapterMenu);
+        View chapterView=findViewById(R.id.topView);
+        chapterMenu.setOnClickListener(view->
+        {
+            if (chapterView.getVisibility()==View.VISIBLE)
+            {
+                chapterView.setVisibility(View.GONE);
+            }
+            else {
+                showChapterMenu();
+            }
+        });
+    }
+    private void showChapterMenu()
+    {
+        View chapterView=findViewById(R.id.topView);
+        chapterView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideChapterMenu()
+    {
+        View chapterView=findViewById(R.id.topView);
+        chapterView.setVisibility(View.GONE);
     }
 
     private void setFontSize(float textSize)
@@ -57,10 +154,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void loadText(TextView textCanvas)
+    private void loadText(TextView textCanvas, String textFile)
     {
         try {
-            InputStream inputStream = getAssets().open("bajrang_baan.txt");
+            InputStream inputStream = getAssets().open(textFile);
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             StringBuilder text = new StringBuilder();
             String line;
